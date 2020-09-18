49ccb5df3f9890d1846b1a178cb42999e7222ba4
Gabriel Deina <gabriel5412@hotmail.com>
2020-09-18 09:15:15 -0300

    Melhorada a lógica do Main

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -2,7 +2,7 @@ public class Triangulo {
 
 	public String tipoTriangulo(double a, double b, double c) {
 
-		if (a == 0 || b == 0 || c == 0 || a < 0 || b < 0 || c < 0) {
+		if (a <= 0 || b <= 0 || c <= 0) {
 			return "Não é um triângulo";
 		}
 		if (a + b > c && a + c > b && b + c > a) {
