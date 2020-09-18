bcc60929f115d1641406c9c848cd35ee5a161fd6
Gabriel Deina <gabriel5412@hotmail.com>
2020-09-18 08:09:54 -0300

    Laço de repetição foi criado na classe Main

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		Triangulo triangulo = new Triangulo();
-
+		int opcao = 1;
+		
+		do {
+			
 		double A = Console.readDouble("Informe o valor de A: ");
 		double B = Console.readDouble("Informe o valor de B: ");
 		double C = Console.readDouble("Informe o valor de C: ");
 
 		System.out.println(triangulo.tipoTriangulo(A, B, C));
+		
+		opcao = Console.readInt("Deseja continuar? 1 SIM | 0 NÃO");	
+		} while (opcao != 0);
 	}
 
 }
